2c93cd39c7be1f15681d6904cfdad431febe08f8
v-kuu <vilku@proton.me>
2026-08-24 21:10:57 +0300

    Added EpollFd

---
+++ src/capture/EpollFd.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <expected>
+#include <utility>
+
+namespace surma::capture
+{
+
+enum class EpollErr
+{
+    CreateErr,
+    CtlErr,
+    EpollErr,
+};
+
+class EpollFd
+{
+  public:
+    EpollFd() = delete;
+    ~EpollFd();
+    EpollFd(const EpollFd &other) = delete;
+    EpollFd &operator=(const EpollFd &other) = delete;
+    EpollFd(EpollFd &&other) noexcept : fd_(std::exchange(other.fd_, -1))
+    {
+    }
+
+    static std::expected<EpollFd, EpollErr> init(int xsk_fd);
+
+  private:
+    explicit EpollFd(int epfd) : fd_(epfd)
+    {
+    }
+    int fd_ = -1;
+};
+
+} // namespace surma::capture
